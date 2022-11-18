6592c582c9703fec47c34e2c52db2ab1b0c83ce1
Ryojin <ryojin.eth@gmail.com>
2022-11-19 09:04:30 +1100

    add carousel

--- src/Organism/Slider.vue
+++ src/Organism/Slider.vue
@@ -1,111 +1,29 @@
 <template>
-  <div class="relative w-full bg-s-charcoal py-4" id="partners">
-    <hooper
-      ref="carousel"
-      class="focus:outline-none h-full"
-      :wheel-control="false"
-      :infinite-scroll="true"
-      :settings="hooperSettings"
-    >
-      <slide>
-        <div
-          class="m-4 flex w-full items-center space-x-4 rounded-md border border-s-darkpurple bg-s-navyLight p-4 text-white"
-        >
-          <div class="w-16 bg-white/10"><img src="/divine.png" alt="" /></div>
-          <div class="w-16 text-sm font-bold">Divine Providence</div>
-        </div>
-      </slide>
-      <slide>
-        <div
-          class="m-4 flex items-center space-x-4 rounded-md border border-s-darkpurple bg-s-navyLight p-4 text-white blur-sm"
-        >
-          <div class="h-16 w-16 bg-white/10" />
-          <div class="text-sm font-bold">Placeholder</div>
-        </div>
-      </slide>
-      <slide>
-        <div
-          class="m-4 flex items-center space-x-4 rounded-md border border-s-darkpurple bg-s-navyLight p-4 text-white blur-sm"
-        >
-          <div class="h-16 w-16 bg-white/10" />
-          <div class="text-sm font-bold">Placeholder</div>
-        </div>
-      </slide>
-      <slide>
-        <div
-          class="m-4 flex items-center space-x-4 rounded-md border border-s-darkpurple bg-s-navyLight p-4 text-white blur-sm"
-        >
-          <div class="h-16 w-16 bg-white/10" />
-          <div class="text-sm font-bold">Placeholder</div>
-        </div>
-      </slide>
-      <slide>
-        <div
-          class="m-4 flex items-center space-x-4 rounded-md border border-s-darkpurple bg-s-navyLight p-4 text-white blur-sm"
-        >
-          <div class="h-16 w-16 bg-white/10" />
-          <div class="text-sm font-bold">Placeholder</div>
-        </div>
-      </slide>
-    </hooper>
+  <div class="bg-s-charcoal">
+    <div class="marquee-content relative w-full py-4" id="partners">
+      <div
+        v-for="i in 5"
+        class="marquee-item m-4 flex items-center space-x-4 rounded-md border border-s-darkpurple bg-s-navyLight p-4 text-white blur-sm"
+      >
+        <div class="h-16 w-16 bg-white/10" />
+        <div class="text-sm font-bold">Placeholder</div>
+      </div>
+      <div
+        class="marquee-item m-4 flex w-full items-center space-x-4 rounded-md border border-s-darkpurple bg-s-navyLight p-4 text-white"
+      >
+        <div class="w-16 bg-white/10"><img src="/divine.png" alt="" /></div>
+        <div class="no-mouse w-16 text-sm font-bold">Divine Providence</div>
+      </div>
+      <div
+        v-for="i in 5"
+        class="marquee-item m-4 flex items-center space-x-4 rounded-md border border-s-darkpurple bg-s-navyLight p-4 text-white blur-sm"
+      >
+        <div class="h-16 w-16 bg-white/10" />
+        <div class="text-sm font-bold">Placeholder</div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-import { Hooper, Slide } from 'hooper';
-import 'hooper/dist/hooper.css';
-
-export default {
-  components: {
-    Hooper,
-    Slide,
-  },
-  props: {
-    noPaginate: {
-      type: Boolean,
-      default: false,
-    },
-    mintSlider: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data() {
-    return {
-      carouselData: 0,
-
-      hooperSettings: {
-        itemsToShow: 1.5,
-        centerMode: true,
-        autoPlay: true,
-        breakpoints: {
-          800: {
-            centerMode: false,
-            itemsToShow: 3.5,
-          },
-          1000: {
-            itemsToShow: 4.5,
-            pagination: 'fraction',
-          },
-        },
-      },
-    };
-  },
-  watch: {
-    carouselData() {
-      this.$refs.carousel.slideTo(this.carouselData);
-    },
-  },
-  methods: {
-    slidePrev() {
-      this.$refs.carousel.slidePrev();
-    },
-    slideNext() {
-      this.$refs.carousel.slideNext();
-    },
-  },
-};
-</script>
 
 <style media="screen">
 .hooper {
@@ -123,5 +41,31 @@ export default {
 }
 .hooper-indicator {
   @apply h-3 w-3 rounded-full;
+}
+.no-mouse {
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
+}
+
+.marquee-content {
+  display: flex;
+  animation: scrolling 50s linear infinite;
+}
+
+.marquee-item {
+  flex: 0 0 16vw;
+  margin: 0 1vw;
+  /*   flex: 0 0 20vw; */
+  /*   margin: 0 2vw; */
+}
+
+@keyframes scrolling {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translatex(-144vw);
+  }
 }
 </style>
