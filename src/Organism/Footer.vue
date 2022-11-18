e9869c9b963c1f1a805ca3eeff930148014428ab
Ryojin <ryojin.eth@gmail.com>
2022-11-18 20:41:47 +1100

    fixes

--- src/Organism/Footer.vue
+++ src/Organism/Footer.vue
@@ -1,18 +1,49 @@
 <template>
   <footer class="bg-s-charcoal">
     <div
-      class="flex flex-col items-center justify-between border-t py-3 px-8 text-xs text-white sm:flex-row xl:px-12"
+      class="flex flex-col items-center justify-between border-t border-b border-gray-400 py-4 px-8 text-center text-xs text-white md:flex-row xl:px-12"
     >
       <div class="flex items-center justify-between space-x-4 text-right">
         <img src="/logo.png" alt="" />
       </div>
-      <div class="space-x-4">
-        <g-link class="custom-transition hover:text-lab-pink" to="/terms">
-          Terms and Conditions</g-link
-        >
-        <g-link class="custom-transition hover:text-lab-pink" to="/privacy">
-          Privacy Policy</g-link
-        >
+      <div
+        class="flex flex-col items-center justify-center md:flex-row md:space-x-6"
+      >
+        <div>
+          <div class="gradientText text-sm font-bold">SUPPORT</div>
+          <div class="space-x-4 text-gray-400">
+            <g-link
+              class="custom-transition hover:text-lab-pink"
+              to="https://dashboard.swifttech.app/
+"
+            >
+              Membership</g-link
+            >
+            <g-link
+              class="custom-transition hover:text-lab-pink"
+              to="https://dashboard.swifttech.app/
+"
+            >
+              Support</g-link
+            >
+          </div>
+        </div>
+        <div class="">
+          <div class="gradientText text-sm font-bold uppercase">Legal</div>
+          <div class="space-x-4 text-gray-400">
+            <g-link class="custom-transition hover:text-lab-pink" to="/terms">
+              Terms and Conditions</g-link
+            >
+            <g-link class="custom-transition hover:text-lab-pink" to="/privacy">
+              Privacy Policy</g-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-full justify-between px-4 py-2 md:flex md:px-12">
+      <div class="p-3 text-center text-xs text-white">
+        © 2022 Swift Tech | All rights reserved
       </div>
       <div
         class="flex w-full items-center justify-center space-x-4 pt-10 sm:justify-end sm:pt-0 md:w-auto"
@@ -30,9 +61,6 @@
           class="text-s-purple"
         />
       </div>
-    </div>
-    <div class="bg-s-navy p-3 text-center text-xs text-white">
-      © 2022 Swift Tech | All rights reserved
     </div>
   </footer>
 </template>
